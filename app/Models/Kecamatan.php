cb04e3f551ff15f42a3e208f0091c51298e738dd
fauzul2002 <fauzulshalis5@gmail.com>
2024-04-24 17:10:16 +0700

    main

--- app/Models/Kecamatan.php
+++ app/Models/Kecamatan.php
@@ -5,13 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Jagung extends Model
+class Kecamatan extends Model
 {
     use HasFactory;
     protected $guarded= ['id'];
 
-    public function Kecamatan()
+    public function Jagung()
     {
-        return $this->belongsTo(Kecamatan::class, 'kecamatan');
+        return $this->hasOne(Jagung::class, 'kecamatan', 'id');
     }
 }
